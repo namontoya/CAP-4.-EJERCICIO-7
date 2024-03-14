06789ca63d0c3472cc2a727f8a12cc2bd3f8a4ec
namontoya <161357317+namontoya@users.noreply.github.com>
2024-03-14 08:29:25 -0500

    Add files via upload

---
+++ 4. EJERCICIO 7.py
@@ -0,0 +1,14 @@
+#Capitulo 4. Ejercicio resuelto 7
+
+A = input("Ingrese el valor A: ")
+B = input("Ingrese el valor B: ")
+
+if A > B:
+    print("A es mayor que B")
+
+elif A == B:
+    print("A es igual que B")
+
+else:
+    print ("A es menor que B")
+    
